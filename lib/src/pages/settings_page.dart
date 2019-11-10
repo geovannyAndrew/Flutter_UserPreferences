07cdc7e9b2048377a2394ad36ab57f5a640ea61d
Geovanny Buitrago <geovanny.andrew911@gmail.com>
2019-11-10 12:53:17 -0500

    Converting settings page to statefull widget, and creting vars in order to save changes in ui.

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_user_preferences/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
 
   static final String routeName = 'settings';
 
-  const SettingsPage({Key key}) : super(key: key);
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _secondaryColor = false;
+  int _gender = 1;
+  String _name = 'Juan';
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    _textController = TextEditingController(
+      text: _name
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +30,63 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferences'),
       ),
-      body: Center(
-        child: Text('Settings'),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: Text('Settings',
+              style: TextStyle(
+                fontSize: 45.0,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+           Divider(),
+           SwitchListTile(
+             value: _secondaryColor,
+             title: Text('Secondary Color'),
+             onChanged: (value){
+               setState(() {
+                 _secondaryColor = value;
+               });
+             },
+           ),
+           RadioListTile(
+             value: 1,
+             groupValue: _gender,
+             title: Text('Male'),
+             onChanged: (value){
+               setState(() {
+                _gender = value;                 
+               });
+             },
+           ),
+           RadioListTile(
+             value: 2,
+             groupValue: _gender,
+             title: Text('Female'),
+             onChanged: (value){
+               setState(() {
+                _gender = value;                 
+               });
+             },
+           ),
+           Divider(),
+           Container(
+             padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+             child: TextField(
+               controller: _textController,
+               decoration: InputDecoration(
+                 labelText: 'Name',
+                 helperText: 'Person\'s Name'
+               ),
+               onChanged: (value){
+                 _name = value;
+               },
+             ),
+           )
+        ],
       ),
       drawer: MenuWidget(),
     );
